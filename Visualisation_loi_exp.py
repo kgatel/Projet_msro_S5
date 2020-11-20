42a38e6c8016c46a265560b17e6de7a5a856cf33
Thibaut <thibaut.andregallis@insa-rouen.fr>
2020-11-20 17:35:44 +0100

    Amélioration du programme
    J'ai viré ce qui ne servait plus et j'ai ajouté l'esperance, le calcul
    de l'ecart entre les courbes et corrigé quelques bugs pour avoir la
    superposition des deux courbes pour tout lambda

--- Visualisation_loi_exp.py
+++ Visualisation_loi_exp.py
@@ -3,31 +3,30 @@ import random
 import matplotlib.pyplot as plt
 import math as m
 
-N=1000
-Lambda=1.5
+N=2000
+Lambda=0.5
+esperance=0.0
+ecart_moyen=0.0
 tab=np.zeros(N)
 for i in range(N):
     tab[i]=random.random()
     tab[i]=(-1/Lambda)*m.log(1-tab[i])
 tabtrie=sorted(tab)
-tabtriedec=sorted(tab,reverse=True)
 
-t2 = np.arange(0.0, 5.0, 5/N)
-t1 = np.arange(0.0,4.0,4.0/N)
+t1 = np.arange(0.0,5.0,5.0/N)
 
 tabcompteur = np.zeros(N)
 
+i=1
 for i in range(N):
     for j in range(N):
-        if (tabtrie[j]<=t1[i])or(tabtrie[j]>=4):
+        if (tabtrie[j]<=t1[i])or((tabtrie[j]>=5.0)and(t1[i]>=5.0)):
             tabcompteur[i]+=1
             
 F=tabcompteur/N
 plt.plot(t1,F,label='F_exp')
 
-f=np.zeros(N)
-
-
+    
 #plt.plot(tabtrie,f,'x')
 
 F_th=np.zeros(N)
@@ -36,11 +35,22 @@ for i in range(N):
 
 plt.plot(tabtrie,F_th,label='F_th')
 
-f_th=np.zeros(N)
-for i in range(N):
-    f_th[i]=Lambda*m.exp(-Lambda*t2[i])
-plt.plot(t2,f_th,label='f_th')
-
 plt.legend()
 plt.axis([0, 5, 0, 2])
 plt.show()
+
+for i in range(N):
+    ecart_moyen+=abs(F[i]-(1-m.exp(-Lambda*t1[i])))
+ecart_moyen=ecart_moyen/N
+print("En calculant l'écart moyen entre les deux courbes on trouve {}".format(round(ecart_moyen,5)))
+print("Donc notre courbe experimentale se superpose presque parfaitement avec la courbe théorique")
+
+for i in range(N):
+    esperance+=tabtrie[i]
+esperance=esperance/N
+
+print()
+print("Nous avons pris λ = {}, notre esperance théorique est donc: 1/λ = {}".format(Lambda, round(1/Lambda,5)))
+print("L'esperance de notre tirage est {}. Ce qui nous donne un écart en valeur absolue de {}".format(round(esperance,5), round(abs(esperance-(1/Lambda)),5)))
+print("Cette différence est toujours très faible, et comfirme que notre tirage se rapproche bien d'un tirage aléatoire")
+print("selon une loi exponentielle de paramètre λ = {}".format(round(Lambda,5)))
