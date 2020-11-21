0cae582db718ddf667d0c4576f7ae0b367aec46a
Kévin Gatel <kevin.gatel@insa'rouen.fr>
2020-11-21 18:09:28 +0100

    Modification du code à cause des accents pour latex + rapport 3.0

---
+++ README.md
@@ -0,0 +1,5 @@
+Projet R0
+ANDRE-GALLIS Thibaut / GATEL Kévin
+GM3
+
+Date de remise : 27 novembre 2020
--- Visualisation_loi_exp.py
+++ Visualisation_loi_exp.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 import math as m
 
 N=2000
-Lambda=0.5
+Lambda=1.5
 esperance=0.0
 ecart_moyen=0.0
 tab=np.zeros(N)
 for i in range(N):
     tab[i]=random.random()
     tab[i]=(-1/Lambda)*m.log(1-tab[i])
-tabtrie=sorted(tab)
+tabtrie=sorted(tab)    #tableau trie
 
-t1 = np.arange(0.0,5.0,5.0/N)
+t1 = np.arange(0.0,5.0,5.0/N)   #tableau N valeurs entre 0 5 par pas 5/5
 
 tabcompteur = np.zeros(N)
 
@@ -26,8 +26,6 @@ for i in range(N):
 F=tabcompteur/N
 plt.plot(t1,F,label='F_exp')
 
-    
-#plt.plot(tabtrie,f,'x')
 
 F_th=np.zeros(N)
 for i in range(N):
@@ -42,15 +40,22 @@ plt.show()
 for i in range(N):
     ecart_moyen+=abs(F[i]-(1-m.exp(-Lambda*t1[i])))
 ecart_moyen=ecart_moyen/N
-print("En calculant l'écart moyen entre les deux courbes on trouve {}".format(round(ecart_moyen,5)))
-print("Donc notre courbe experimentale se superpose presque parfaitement avec la courbe théorique")
+
+print("En calculant l'ecart moyen entre les deux courbes on trouve ")
+print(round(ecart_moyen,5))
+print("Donc notre courbe experimentale se superpose presque ")
+print("parfaitement avec la courbe theorique")
 
 for i in range(N):
     esperance+=tabtrie[i]
 esperance=esperance/N
 
 print()
-print("Nous avons pris λ = {}, notre esperance théorique est donc: 1/λ = {}".format(Lambda, round(1/Lambda,5)))
-print("L'esperance de notre tirage est {}. Ce qui nous donne un écart en valeur absolue de {}".format(round(esperance,5), round(abs(esperance-(1/Lambda)),5)))
-print("Cette différence est toujours très faible, et comfirme que notre tirage se rapproche bien d'un tirage aléatoire")
-print("selon une loi exponentielle de paramètre λ = {}".format(round(Lambda,5)))
+print("Nous avons pris lambda = ",Lambda)
+print("Notre esperance theorique est donc: 1/lambda = ",round(1/Lambda,5))
+print("Ce qui nous donne un ecart en valeur absolue de ")
+print(round(abs(esperance-(1/Lambda)),5))
+print("Cette difference est toujours tres faible, et comfirme ")
+print("que notre tirage se rapproche bien d'un tirage aleatoire ")
+print("selon une loi exponentielle de parametre lambda = ")
+print(format(round(Lambda,5)))
